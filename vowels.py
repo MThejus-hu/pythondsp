56557c2cb1bd7925f4b23f7a9a45505d662ef91b
MThejus-hu <thejusgireesh26@gmail.com>
2026-07-30 10:43:23 +0530

    Delete vowels.py

--- vowels.py
+++
@@ -1,8 +0,0 @@
-text = input("Enter a string: ")
-vowels = "aeiouAEIOU"
-vowel_count = 0
-
-for char in text:
-    if char in vowels:
-        vowel_count += 1
-print(f"Number of vowels: {vowel_count}")
